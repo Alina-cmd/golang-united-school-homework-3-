7c4f86f66d0e34d7a2f778ad48fe0c7bc9ad7ddc
Alina <you@example.com>
2022-05-11 13:06:30 +0300

    hw_3

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,26 @@
 package homework
 
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
+	var minn int
+	for key, _ := range input {
+		minn = key
+		break
+	}
+	i := 0
+	length := len(input)
+	for i < length {
+		for key, _ := range input {
+			if minn > key {
+				minn = key
+			}
+		}
+		result = append(result, input[minn])
+		i++
+		delete(input, minn)
+		for key, _ := range input {
+			minn = key
+			break
+		}
+	}
 	return
 }
